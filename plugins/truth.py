602354d02006fef9e8658b8cfbf67776c60e7335
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-08 05:07:58 +0530

    Update truth.py

--- plugins/truth.py
+++ plugins/truth.py
@@ -1,7 +1,7 @@
 import requests
 from pyrogram import filters
 
-from VIPMUSIC import app
+from ANNIEMUSIC import app
 
 truth_api_url = "https://api.truthordarebot.xyz/v1/truth"
 dare_api_url = "https://api.truthordarebot.xyz/v1/dare"
