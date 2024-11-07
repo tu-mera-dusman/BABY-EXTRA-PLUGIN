a03ff53fe89e746096bfc34da2729e8192337e6d
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-08 04:45:48 +0530

    Update gemini.py

--- plugins/gemini.py
+++ plugins/gemini.py
@@ -2,7 +2,7 @@ import requests
 from MukeshAPI import api
 from pyrogram import filters
 from pyrogram.enums import ChatAction
-from VIPMUSIC import app
+from ANNIEMUSIC import app
 
 
 @app.on_message(filters.command(["gemini"]))
