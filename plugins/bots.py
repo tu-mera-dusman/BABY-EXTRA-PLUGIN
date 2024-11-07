7ca098109225b507364df93f1eabb51d5e68e5a8
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-08 04:30:33 +0530

    Update bots.py

--- plugins/bots.py
+++ plugins/bots.py
@@ -3,7 +3,7 @@ import asyncio
 from pyrogram import enums, filters
 from pyrogram.errors import FloodWait
 
-from VIPMUSIC import app
+from ANNIEMUSIC import app
 
 
 @app.on_message(filters.command("bots") & filters.group)
