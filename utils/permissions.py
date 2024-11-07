482960a3117b3e8865136abff242fbb67e8c066f
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-08 04:21:05 +0530

    Update permissions.py

--- utils/permissions.py
+++ utils/permissions.py
@@ -5,8 +5,8 @@ from traceback import format_exc as err
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
 
-from VIPMUSIC import app
-from VIPMUSIC.misc import SUDOERS
+from ANNIEMUSIC import app
+from ANNIEMUSIC.misc import SUDOERS
 
 
 async def member_permissions(chat_id: int, user_id: int):
